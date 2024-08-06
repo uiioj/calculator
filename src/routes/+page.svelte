df4d9a3053ed1a774d9db0e9d995191914f1f1fd
Shaima <shaimafuadi45@gmail.com>
2024-08-06 13:22:16 +0300

    calculator numbers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,30 @@
         آلة حاسبة
     </title>
  </svetle:head>
- <div class=" bg-white w-[20rem] h-[30rem] rounded-3xl  flex justify-center items-center" > 
-  <div class= "bg-blue-200  w-[10rem] h-[10rem] rounded-full">
 
-  </div>
+ <div class=" bg-white w-[20rem] h-[30rem] rounded-3xl grid grid-cols-4" > 
+   <button>C</button>
+   <button>÷</button>
+   <button>×</button>
+   <button>CE</button>
+   <button>7</button>
+   <button>8</button>
+   <button>9</button>
+   <button>-</button>
+   <button>4</button>
+   <button>5</button>
+   <button>6</button>
+   <button>+</button>
+   <button>1</button>
+   <button>2</button>
+   <button>3</button>
+   <button>.</button>
+   <button>%</button>
+   <button>0</button>
+   <button>√</button>
+   <button>=</button>
+
+  
  </div>
 
 
