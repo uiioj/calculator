0ee2e7de453705cffa5eca4aa18daf4d9e602905
Shaima <shaimafuadi45@gmail.com>
2024-08-10 07:32:39 +0300

    check

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
    import SubtractionIcon from '$lib/icons/SubtractionIcon.svelte';
    import PlusIcon from '$lib/icons/PlusIcon.svelte'
    import EquaLIcon from '../lib/icons/EquaLIcon.svelte';
+   import {onMount} from "svelte";
 
    function addToEquation(value:string){
       equation += value;
@@ -47,9 +48,28 @@
 
   let equation: string = "";
 
-  function onKeyDown(){
-   console.log('hi');
+  function onKeyDown(e: KeyboardEvent){
+  
+  let button = document.getElementById(e.key);
+  button?.click();
+  button?.focus();
+  setTimeout(() => {
+   // @ts-ignore
+            document.activeElement?.blur();
+         } , 200 );
   }
+  onMount(() => {
+ 
+   let allButtons = document.getElementsByTagName('button');
+ console.log(allButtons); 
+ for(let i = 0;i< allButtons.length;i++ ){
+   allButtons[i].addEventListener('click', () => {
+      new Audio('/click.mp3').play();
+
+   })
+
+ }
+  });
      
  </script>
 
@@ -75,39 +95,38 @@
  class="bg-[#dfe3f1] text-end rounded-3xl  col-span-4 min-h-14
   flex  items-center px-5 mb-3 shadow-xl break-all transition-all "> {equation} </div>
 
-   <button on:click={() => clear()} class="bg-[#990033] hover:bg-[#990033]/80 text-lg ">AC</button> <!--مسح الكل-->
+   <button id="Delete" on:click={() => clear()} class="bg-[#990033] hover:bg-[#990033]/80 text-lg ">AC</button> <!--مسح الكل-->
+   <button id="/" on:click={() => addToEquation(' / ')} class="" > <DivideIcon/> </button> 
 
-   <button on:click={() => addToEquation(' / ')} class="bg-[#5e626e]" > <DivideIcon/> </button> 
+   <button id="*" on:click={() => addToEquation(' * ')} class="text-sm "> <X_Icon/> </button> 
 
-   <button on:click={() => addToEquation(' * ')} class="text-sm bg-[#5e626e]"> <X_Icon/> </button> 
+   <button id="Backspace" on:click={() => backSpace( ) } class="bg-[#990033] hover:bg-[#990033]/80  text-2xl "> <DeleteIcon/> </button> 
 
-   <button on:click={() => backSpace( ) } class="bg-[#990033] hover:bg-[#990033]/80  text-2xl "> <DeleteIcon/> </button> 
+   <button id="7" on:click={() => addToEquation('7')}>7</button>
+   <button id="8" on:click={() => addToEquation('8')}>8</button>
+   <button id="9" on:click={() => addToEquation('9')}>9</button>
 
-   <button on:click={() => addToEquation('7')}>7</button>
-   <button on:click={() => addToEquation('8')}>8</button>
-   <button on:click={() => addToEquation('9')}>9</button>
+   <button id="-" on:click={() => addToEquation(' - ')} class="">  <SubtractionIcon/> </button> 
 
-   <button on:click={() => addToEquation(' - ')} class="bg-[#5e626e]">  <SubtractionIcon/> </button> 
+   <button id="4" on:click={() => addToEquation('4')}>4</button>
+   <button id="5" on:click={() => addToEquation('5')}>5</button>
+   <button id="6" on:click={() => addToEquation('6')}>6</button>
 
-   <button on:click={() => addToEquation('4')}>4</button>
-   <button on:click={() => addToEquation('5')}>5</button>
-   <button on:click={() => addToEquation('6')}>6</button>
+   <button id="+" on:click={() => addToEquation(' + ')} class="text-lg "> <PlusIcon/>  </button> 
 
-   <button on:click={() => addToEquation(' + ')} class="text-lg bg-[#5e626e]"> <PlusIcon/>  </button> 
-
-   <button on:click={() => addToEquation('1')}>1</button>
-   <button on:click={() => addToEquation('2')}>2</button>
-   <button on:click={() => addToEquation('3')}>3</button>
+   <button id="1" on:click={() => addToEquation('1')}>1</button>
+   <button id="2" on:click={() => addToEquation('2')}>2</button>
+   <button id="3" on:click={() => addToEquation('3')}>3</button>
 
 
-   <button  on:click={() => addToEquation(' /100 ')} class="bg-[#5e626e]">%</button>
+   <button id="%" on:click={() => addToEquation(' /100 ')} class="">%</button>
 
-   <button on:click={() => addToEquation('.')}>.</button>
-   <button on:click={() => addToEquation('0')}>0</button>
+   <button id="." on:click={() => addToEquation('.')}>.</button>
+   <button id="0" on:click={() => addToEquation('0')}>0</button>
    
    
 
-   <button on:click={() => solve()} class="text-2xl col-span-2 "> <EquaLIcon/> </button>
+   <button id="=" on:click={() => solve()} class="text-2xl col-span-2 "> <EquaLIcon/> </button>
 
   
  </div>
